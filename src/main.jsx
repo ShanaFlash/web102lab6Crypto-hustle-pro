6a0c6d46e6e005468fd7ad47341451aa718a0660
ShanaFlash <nancy17flash@gmail.com>
2023-10-21 22:40:28 -0400

    layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
+import Layout from './routes/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
+<BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index={true} element={<App />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
 )
+
+
